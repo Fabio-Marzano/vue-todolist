6444728f20b0ddc40bd44ee45c7bfbbb5070c95d
fabiomarzano <fabiomarzano1993@gmail.com>
2024-07-18 15:55:05 +0200

    [UPD] create-array-with-objects

--- js/script.js
+++ js/script.js
@@ -1,1 +1,36 @@
+const { createApp, ref } = Vue
 
+createApp({
+    data() {
+        return {
+            
+
+            toodlist: [//array di oggetti
+                {
+                    
+                    done: true,
+                    text: 'Andare a lavoro',
+                }, {
+                    
+                    done: false,
+                    text: 'Giocare a calcio',
+                }, {
+                    
+                    done: false,
+                    text: "Leggere un libro",
+                }, {
+                   
+                    done: false,
+                    text: 'Andare al cinema a vedere Harry Potter',
+                }, {
+                    
+                    done: false,
+                    text: 'Andare allo stadio a tifare la Juve',
+                }
+            ]
+        }
+    },
+    methods: {
+        
+    }
+}).mount('#app')
